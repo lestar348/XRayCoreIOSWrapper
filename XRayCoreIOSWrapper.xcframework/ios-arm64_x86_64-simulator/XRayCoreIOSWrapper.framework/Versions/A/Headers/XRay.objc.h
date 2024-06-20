79a8e59ac7a7ef08e64b5df06148e7db8423f86d
k.perminov <lestar348@gmail.com>
2024-06-20 10:58:42 +0300

    added ping

--- XRayCoreIOSWrapper.xcframework/ios-arm64_x86_64-simulator/XRayCoreIOSWrapper.framework/Versions/A/Headers/XRay.objc.h
+++ XRayCoreIOSWrapper.xcframework/ios-arm64_x86_64-simulator/XRayCoreIOSWrapper.framework/Versions/A/Headers/XRay.objc.h
@@ -19,6 +19,11 @@
 @end
 
 /**
+ * / Real ping
+ */
+FOUNDATION_EXPORT BOOL XRayMeasureOutboundDelay(NSData* _Nullable config, NSString* _Nullable url, int64_t* _Nullable ret0_, NSError* _Nullable* _Nullable error);
+
+/**
  * Ser AssetsDirectory in Xray env
  */
 FOUNDATION_EXPORT void XRaySetAssetsDirectory(NSString* _Nullable path);
