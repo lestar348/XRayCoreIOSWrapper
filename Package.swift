07ccf3890b8c317e75399213869c57212b6032d7
k.perminov <lestar348@gmail.com>
2024-08-29 15:19:47 +0300

    add platforms to packege

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "XRayCoreIOSWrapper",
+    platforms: [
+        .iOS(.v13),
+        .macCatalyst(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
