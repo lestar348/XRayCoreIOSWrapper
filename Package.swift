89995e949c0a5397b188f7832d057ae1588e8d0f
k.perminov <lestar348@gmail.com>
2024-09-10 10:51:38 +0300

    add macos

--- Package.swift
+++ Package.swift
@@ -5,8 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "XRayCoreIOSWrapper",
-    platforms: [
-        .iOS(.v13)],
+    platforms: [.iOS(.v13), .macOS(.v12)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
